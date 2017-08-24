f47421409b22760510d5c4e77e78b8711b8413f1
PhanVanLinh <phanvanlinh.94vn@gmail.com>
2017-08-24 10:35:26 +0700

    first commit

---
+++ app/src/main/java/com/toong/androidbindingcustomview/CustomView.java
@@ -0,0 +1,59 @@
+package com.toong.androidbindingcustomview;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class CustomView extends LinearLayout {
+
+    private ImageView imgImage;
+    private TextView tvText;
+
+    public CustomView(Context context) {
+        this(context, null);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        LayoutInflater.from(getContext()).inflate(R.layout.custom_layout, this, true);
+
+        imgImage = (ImageView) findViewById(R.id.image);
+        tvText = (TextView) findViewById(R.id.text);
+
+        TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.CustomViewStyle);
+        String text = ta.getString(R.styleable.CustomViewStyle_ib_text);
+
+        Drawable drawable = ta.getDrawable(R.styleable.CustomViewStyle_image);
+
+        ta.recycle();
+        if (drawable != null) {
+            imgImage.setBackground(drawable);
+        }
+        setIb_text(text);
+    }
+
+    public void setIbText(String text){
+        tvText.setText(text);
+    }
+
+//    @BindingAdapter("setIb_text"
+    public void setIb_text(String text){
+        tvText.setText(text);
+    }
+
+}
---
+++ app/src/main/java/com/toong/androidbindingcustomview/MainViewModel.java
@@ -0,0 +1,11 @@
+package com.toong.androidbindingcustomview;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by PhanVanLinh on 18/07/2017.
+ * phanvanlinh.94vn@gmail.com
+ */
+
+public class MainViewModel extends BaseObservable{
+}
